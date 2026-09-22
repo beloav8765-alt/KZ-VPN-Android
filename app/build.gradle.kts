c5a9ba74b5180e80f22ed986753ee94fcd6c110e
beloav8765-alt <beloav8765@gmail.com>
2026-09-22 14:19:09 +0500

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.kzvpn.app"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.kzvpn.app"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 4
+        versionName = "0.3.1"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+
+    packaging {
+        resources.excludes += setOf(
+            "/META-INF/{AL2.0,LGPL2.1}",
+            "META-INF/LICENSE*",
+            "META-INF/NOTICE*"
+        )
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    implementation("com.wireguard.android:tunnel:1.0.20260102")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+}
