9d3a0736e1680849e7386129950d3a74812186b7
beloav8765-alt <beloav8765@gmail.com>
2026-09-22 17:28:27 +0500

    Bump KZ VPN to 0.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kzvpn.app.v032"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.3.3"
+        versionCode = 7
+        versionName = "0.3.4"
     }
 
     buildFeatures {
